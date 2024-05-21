b58da18cfbbb978f58351d24d89fb4271b4ecc6c
Arun1only1 <arun1nly1@gmail.com>
2024-05-21 13:20:37 +0545

    minor change

--- git_commands.js
+++ git_commands.js
@@ -3,6 +3,7 @@
 // git add  . => add file to git to track
 // git commit -m <message> =>to commit code on git
 // to see remote => git remote -v
+
 //  to add remote => git remote add <name> <link>
 // git branch -M main
 //  git push -u <name> <branch>
